2f67365ed36b3dcc336c0db3a7e94fe12b5b34b8
Davi Oliveira <davioliveira541@outlook.com>
2025-02-08 20:58:40 -0300

    update

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# EmbarcaTech-TarefaU4C6
- 
+# Projeto: Controle de LEDs e Display com RP2040
+
+Este projeto implementa um sistema de controle de LEDs e display utilizando o microcontrolador RP2040, uma matriz de LEDs WS2812, um LED RGB, botões de acionamento e um display SSD1306. O sistema permite a interação com os LEDs e o display através de botões e entrada de caracteres via PC.
+
+## Requisitos
+
+Este projeto envolve a modificação da biblioteca font.h para adicionar caracteres minúsculos, entrada de caracteres via PC para exibição no display SSD1306, interação com os botões A e B para alternar o estado dos LEDs RGB Verde e Azul, respectivamente, uso de interrupções para funcionalidades relacionadas aos botões, implementação de debouncing via software, controle de LEDs comuns e LEDs WS2812, utilização do display 128x64 com fontes maiúsculas e minúsculas, envio de informações pela UART, e organização do código de forma estruturada e comentada.
+
+## Compilação e Execução
+
+1. Clone o repositório do projeto.
+2. Configure o ambiente de desenvolvimento do Raspberry Pi Pico.
+3. Compile o código usando o CMake.
+4. Carregue o binário no Raspberry Pi Pico.
+
+## Estrutura do Código
+
+### Função **desenho_pio**
+A função **desenho_pio** é responsável por acionar a matriz de LEDs WS2812. Ela converte os valores RGB em um único valor de 32 bits e envia os dados para a matriz de LEDs.
+
+### Função **gpio_irq_handler**
+A função **gpio_irq_handler** é chamada quando um dos botões é pressionado. Ela alterna o estado dos LEDs RGB e exibe uma mensagem no display SSD1306 e no Serial Monitor.
+
+### Função **main**
+A função **main** inicializa a comunicação serial, os pinos dos LEDs e botões, e configura o display SSD1306. Em seguida, entra em um loop infinito onde lê caracteres do Serial Monitor e atualiza a matriz de LEDs e o display conforme necessário.
+
+### Debounce do Botão
+Para evitar múltiplas leituras do botão devido ao efeito bouncing, foi implementada uma rotina de debounce. Após detectar um clique no botão, o programa espera 300 ms antes de processar a próxima interrupção.
+
+### Link para o vídeo de explicação
--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include "inc/ssd1306.h"
 #include "inc/font.h"
 #include "hardware/clocks.h"
+#include "pio_matrix.pio.h"
 
 #define I2C_PORT i2c1
 #define I2C_SDA 14
@@ -17,24 +18,22 @@
 #define UART_ID uart0    // Seleciona a UART0
 #define BAUD_RATE 115200 // Define a taxa de transmissão
 
-#define LED_PIN_GREEN 11
-#define LED_PIN_RED 13
-#define LED_PIN_BLUE 12
 #define PIN_BUTTON_A 5
 #define PIN_BUTTON_B 6
 #define OUT_PIN 7
-
-
-#include "pio_matrix.pio.h"
-
+#define LED_PIN_GREEN 11
+#define LED_PIN_BLUE 12
+#define LED_PIN_RED 13
 
 #define DEBOUNCE_TIME_MS 300 // Tempo de debounce em milissegundos
+
 absolute_time_t last_interrupt_time = {0};
 ssd1306_t ssd;
 
 bool led_verde_ligado = false;
 bool led_azul_ligado = false;
 
+// Matriz de números para exibição
 double matrizNumeros[11][25] ={
     // 0
     {0.0, 0.1, 0.1, 0.1, 0.0,
@@ -105,6 +104,7 @@ double matrizNumeros[11][25] ={
       0.0, 0.0, 0.0, 0.0, 0.0}
 };
 
+// Função para converter valores RGB em um único valor de 32 bits
 uint32_t matrix_rgb(double r, double g, double b)
 {
   unsigned char R, G, B;
@@ -114,26 +114,26 @@ uint32_t matrix_rgb(double r, double g, double b)
   return (G << 24) | (R << 16) | (B << 8);
 }
 
-//rotina para acionar a matrix de leds - ws2812b
-void desenho_pio(double *desenho, uint32_t valor_led, PIO pio, uint sm, double r, double g, double b){
-
+// Rotina para acionar a matriz de LEDs - ws2812b
+void desenho_pio(double *desenho, uint32_t valor_led, PIO pio, uint sm, double r, double g, double b)
+{
     for (int16_t i = 0; i < NUM_PIXELS; i++) {
-        if (i%2==0)
+        if (i % 2 == 0)
         {
-            valor_led = matrix_rgb(r, g, desenho[24-i]);
-            pio_sm_put_blocking(pio, sm, valor_led);
-
-        }else{
-            valor_led = matrix_rgb(r, desenho[24-i], b);
-            pio_sm_put_blocking(pio, sm, valor_led);
+          valor_led = matrix_rgb(r, g, desenho[24 - i]);
+          pio_sm_put_blocking(pio, sm, valor_led);
+        }
+        else
+        {
+          valor_led = matrix_rgb(r, desenho[24 - i], b);
+          pio_sm_put_blocking(pio, sm, valor_led);
         }
     }
 }
+
+// Função de tratamento de interrupção para os botões
 static void gpio_irq_handler(uint gpio, uint32_t events)
 {
-  
-  //ssd1306_fill(&ssd, false); // Limpa o display
-
   // Obter o tempo atual para o debounce
   absolute_time_t current_time = get_absolute_time();
 
@@ -146,6 +146,7 @@ static void gpio_irq_handler(uint gpio, uint32_t events)
     last_interrupt_time = current_time;
   }
 
+  // Verifica qual botão foi pressionado e alterna o estado do LED correspondente
   if (gpio == PIN_BUTTON_A)
   {
     printf("Botão A pressionado\n");
@@ -165,8 +166,6 @@ static void gpio_irq_handler(uint gpio, uint32_t events)
   }
   else if (gpio == PIN_BUTTON_B)
   {
-    printf("Botão B pressionado\n");
-
     if (led_azul_ligado)
     {
       printf("O led azul foi desligado.\n");
@@ -175,12 +174,13 @@ static void gpio_irq_handler(uint gpio, uint32_t events)
     }
     else
     {
-      printf("O led azul foi ligado.\n");
+      printf("O led azul foi desligado.\n");
       ssd1306_draw_string(&ssd, "LED Azul  ON ", 0, 10);
       led_azul_ligado = true;
     }
   }
 
+  // Atualiza o estado dos LEDs
   gpio_put(LED_PIN_GREEN, led_verde_ligado);
   gpio_put(LED_PIN_BLUE, led_azul_ligado);
   ssd1306_send_data(&ssd);
@@ -188,20 +188,18 @@ static void gpio_irq_handler(uint gpio, uint32_t events)
 
 int main()
 {
+  // Inicialização do PIO
   PIO pio = pio0; 
-  bool ok;
-  uint16_t i;
-  uint32_t valor_led;
-  double r = 0.0, b = 0.0 , g = 0.0;
-  
+  uint32_t valor_led;  
+
   stdio_init_all();
 
-  //configurações da PIO
+  // Configurações da PIO
   uint offset = pio_add_program(pio, &pio_matrix_program);
   uint sm = pio_claim_unused_sm(pio, true);
   pio_matrix_program_init(pio, sm, offset, OUT_PIN);
-  i2c_init(I2C_PORT, 400 * 1000);
   
+  // Inicialização dos pinos dos botões e LEDs
   gpio_init(PIN_BUTTON_A);
   gpio_init(PIN_BUTTON_B);
   gpio_init(LED_PIN_GREEN);
@@ -212,15 +210,21 @@ int main()
   gpio_set_dir(LED_PIN_BLUE, GPIO_OUT);
   gpio_pull_up(PIN_BUTTON_A);
   gpio_pull_up(PIN_BUTTON_B);
-
+  
+  // Configuração das interrupções dos botões
   gpio_set_irq_enabled_with_callback(PIN_BUTTON_A, GPIO_IRQ_EDGE_FALL, 1, &gpio_irq_handler);
   gpio_set_irq_enabled_with_callback(PIN_BUTTON_B, GPIO_IRQ_EDGE_FALL, 1, &gpio_irq_handler);
+  
+  // Inicialização do I2C
+  i2c_init(I2C_PORT, 400 * 1000);
 
-
+  // Configuração do I2C para o display SSD1306
   gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
   gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
   gpio_pull_up(I2C_SDA);
   gpio_pull_up(I2C_SCL);
+  
+  // Configuração do display SSD1306 
   ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT);
   ssd1306_config(&ssd);
   ssd1306_send_data(&ssd);
@@ -229,25 +233,31 @@ int main()
   ssd1306_fill(&ssd, false);
   ssd1306_send_data(&ssd);
 
-  // Limpa a matriz de leds 
+  // Limpa a matriz de LEDs
   desenho_pio(matrizNumeros[10], valor_led, pio, sm, 0, 0, 0);
-
 
   while (true)
   {
     char str[2];
-    fgets(str, sizeof(str), stdin); // Lê uma string de até 99 caracteres + '\0'
+    // Lê a string digitada pelo usuário
+    fgets(str, sizeof(str), stdin);
+    // Lê o primeiro caractere da string
     char c = str[0];
+
+    // Verifica se o caractere é um número e atualiza a matriz de LEDs
     if(c >= '0' && c <= '9')
     {
-      desenho_pio(matrizNumeros[c - '0'], valor_led, pio, sm, r, g, b);
+      desenho_pio(matrizNumeros[c - '0'], valor_led, pio, sm, 0, 0, 0);
     }
     else
     {
       desenho_pio(matrizNumeros[10], valor_led, pio, sm, 0, 0, 0);
     }
-    ssd1306_draw_string(&ssd, &c, 60, 25); // Desenha uma string
+
+    // Desenha o caractere no display
+    ssd1306_draw_string(&ssd, &c, 60, 25);
     ssd1306_send_data(&ssd); // Atualiza o display
+    // Aguarda 500ms
     sleep_ms(500);
   }
 }
